1539ee065a58e97050ca27514cab956007cf28ef
MingyeomKim <mingyum119@naver.com>
2023-09-29 19:31:20 +0900

    feat: Scafold 위젯을 사용해 앱 레이아웃 만들기

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -8,14 +8,30 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) { // 위젯을 사용하여
+  Widget build(BuildContext context) {
     return MaterialApp(
-      // home: Icon(Icons.stars)
-      // home: Text("텍스트")
-      // home: Image.asset('emoticon.png')
-      home: Center(
-        child: Container( width: 50, height : 50, color: Colors.blue)
-      )
+      home: Scaffold( // 상중하 레이아웃으로 나누는 위젯
+        appBar: AppBar(
+          title: Text('앱임'),
+        ),
+        body: Container(
+          child: Text('안녕')
+        ),
+        bottomNavigationBar:
+          BottomAppBar(
+            child: Container (
+                height : 60,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children : [
+                      Icon(Icons.phone),
+                      Icon(Icons.message),
+                      Icon(Icons.contact_page)
+                    ]
+                )
+            )
+          )
+      ),
     );
   }
 }
