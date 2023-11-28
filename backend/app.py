67da75465900cafa1a8ea738f47a3bcbf57e0277
MingyeomKim <mingyum119@naver.com>
2023-11-28 21:25:33 +0900

    feat: pytorch 모델 배포하는 코드 작성

--- backend/app.py
+++ backend/app.py
@@ -2,60 +2,63 @@ import os
 import cv2
 import numpy as np
 import pickle
+import torch
+from torchvision import transforms
+from PIL import Image
+from werkzeug.utils import secure_filename
 
 # Keras
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
 
 # Flask utils
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
-# Load model
-MODEL_PATH = './backend/models/model.h5'
-model = load_model(MODEL_PATH)
-model.make_predict_function()
-
-PKL_PATH = './backend/models/label_encoder.pkl'
-with open(PKL_PATH, 'rb') as f: 
-    label_encoder = pickle.load(f)
-
-def model_predict(img_path, model):
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (224, 224))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = img / 255.0
-
-    preds = model.predict(img)
-    return preds
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        f = request.files['file']
+# loading model
+MODEL_PATH = 'models/aram_model1.pt'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = torch.load(MODEL_PATH, map_location=device)
+model.eval()
 
-        # Save the file
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+# Image preprocessing transformation
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-        # Make prediction
-        preds = model_predict(file_path, model)
+def preprocess_image(image):
+    img = Image.open(image).convert("RGB")
+    img = transform(img)
+    img = img.unsqueeze(0)
+    return img
 
-        # Process the result
-        pred_class = np.argmax(preds)
-        class_label = label_encoder.inverse_transform([pred_class])
-        result = "Predicted class : " + class_label[0]
-        return result
-    return None
+# download image
+DOWNLOAD_FOLDER = 'uploads'
+@app.route('/predict', methods=['POST'])
+def upload(): 
+    # 클라이언트로부터 이미지를 받아와 서버에 저장
+    file = request.files['file']
+
+    # 서버에 이미지 파일 저장
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(DOWNLOAD_FOLDER, filename))
+    
+    input_image = preprocess_image(file)
+    with torch.no_grad():
+        input_image = input_image.to(device)
+        output = model(input_image)
+    
+    _, predicted_class = torch.max(output, 1)
+    result = int(predicted_class.item())
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
